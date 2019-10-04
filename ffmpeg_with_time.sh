9f74b663e476501462a9a9d12186b96183d2a1a7
Arminas <arminas.grigalius@gmail.com>
2019-10-04 12:26:27 +0100

    Lower resolution for transcoding

--- ffmpeg_with_time.sh
+++ ffmpeg_with_time.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ffmpeg -f v4l2 -input_format yuv420p -s 1024x768 -framerate 15 -an -i /dev/video0 \
-       -vf "drawtext=fontfile=/usr/share/fonts/dejavu/DejaVuSans-Bold.ttf: text='%{localtime\:%T}': fontcolor=white@0.8: x=7: y=700" \
+ffmpeg -f v4l2 -input_format yuv420p -s 640x480 -framerate 15 -an -i /dev/video0 \
+       -vf "drawtext=fontfile=/usr/share/fonts/dejavu/DejaVuSans-Bold.ttf: text='%{localtime\:%T}': fontcolor=white@0.8: x=10: y=10" \
        -c:v h264 -an -g 15 -sc_threshold 0 -f flv rtmp://localhost/show/1
 
