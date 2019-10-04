795a4fb7d450a2d1cd8b0552eb4f3e6062c0b75b
Arminas <arminas.grigalius@gmail.com>
2019-10-04 07:47:03 +0100

    Tweak ffmpeg parameters

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ffmpeg -f v4l2 -input_format h264 -r 25 -s 1024x768 -i /dev/video0 -c:v copy -an -f flv rtmp://localhost/show/1
+ffmpeg -f v4l2 -input_format h264 -r 25 -s 1024x768 -i /dev/video0 -c:v copy -an -g 25 -sc_threshold 0 -f flv rtmp://localhost/show/1
 
