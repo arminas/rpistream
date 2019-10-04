4194a942c162146bbccd1eebe96fd60fa260d632
Arminas <arminas.grigalius@gmail.com>
2019-10-04 19:05:48 +0100

    seed as separate docker node

--- seed/ffmpeg_with_time.sh
+++ seed/ffmpeg_with_time.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
-ffmpeg -f v4l2 -input_format yuv420p -s 640x480 -framerate 15 -an -i /dev/video0 \
+ffmpeg -hide_banner \
+       -f v4l2 -input_format yuv420p -s 640x480 -framerate 15 -an -i /dev/video0 \
        -vf "drawtext=fontfile=/usr/share/fonts/dejavu/DejaVuSans-Bold.ttf: text='%{localtime\:%T}': fontcolor=white@0.8: x=10: y=10" \
-       -c:v h264 -an -g 15 -sc_threshold 0 -f flv rtmp://localhost/show/1
+       -c:v h264 -an -g 15 -sc_threshold 0 -f flv rtmp://streamer/show/1
 
