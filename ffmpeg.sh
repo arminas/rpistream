223693772e36cce0fa1685504e5026ed79827b57
Arminas <arminas.grigalius@gmail.com>
2019-10-04 12:18:22 +0100

    Show time in video (adds CPU penalty)

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-ffmpeg -f v4l2 -input_format h264 -r 25 -s 1024x768 -i /dev/video0 -c:v copy -an -g 25 -sc_threshold 0 -f flv rtmp://localhost/show/1
+ffmpeg -f v4l2 -input_format h264 -s 1024x768 -framerate 15 -an -i /dev/video0 \
+	-c:v copy -an -g 15 -sc_threshold 0 -f flv rtmp://localhost/show/1
 
